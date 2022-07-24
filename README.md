01611c07d1685d7728eb0b5168be0fd73fc4a187
visheshmohan <vishesh.mohan@centurylink.com>
2022-07-24 07:11:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-sk5yu1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-sk5yu1)
---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ChartOptions } from 'chart.js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent {
+  title = 'ng2-charts-demo';
+
+  // Pie
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: false,
+  };
+  public pieChartLabels = [ [ 'Download', 'Sales' ], [ 'In', 'Store', 'Sales' ], 'Mail Sales' ];
+  public pieChartDatasets = [ {
+    data: [ 300, 500, 100 ]
+  } ];
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
+
+  constructor() {
+  }
+
+}
